75379d5c7aeaaa11b6bcae0c1d38ccb429f1c641
cahthuranag <chathurangab@sltc.ac.lk>
2023-05-09 13:42:39 +0100

    pyplot

--- test/test_plot.py
+++ test/test_plot.py
@@ -1,14 +1,15 @@
 import matplotlib
 matplotlib.use('Agg')  # use Agg backend
+import matplotlib.pyplot as plt
 
 from agenet.plot import plot
 
 def test_plot():
     # call the function to be tested
-    plot(numevnts=1000, numruns=1)
+    plot(numevnts=500, numruns=1)
 
     # assert that the plot is not displayed
-    import matplotlib.pyplot as plt
     assert len(plt.get_fignums()) == 0
+    plt.close('all')
 
 
