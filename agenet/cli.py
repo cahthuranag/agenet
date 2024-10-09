eaf0a2a6d2484a8c2e8462ff13299dd12f7c04e2
cahthuranag <chathurangab@sltc.ac.lk>
2024-10-09 15:55:05 +0100

    Adapt cli function to new multi_param_ev_sim

--- agenet/cli.py
+++ agenet/cli.py
@@ -1,162 +1,75 @@
-"""Command-line interface for the agenet package."""
-
 import argparse
-
+from typing import List
 from .bler import blercal_th
 from .snratio import snr_th
-
+from .maincom import multi_param_ev_sim
 
 def _main() -> None:
-    """Command-line arguments and calls the printage function."""
+    """Command-line arguments and calls the multi_param_ev_sim function."""
     parser = argparse.ArgumentParser(
-        description="Prints a table comparing results for different input values."
+        description="Run multi-parameter event simulation and return results as a DataFrame."
     )
-    parser.add_argument(
-        "--num_nodes_const",
-        type=int,
-        default=2,
-        help="Constant value for the number of nodes.",
-    )
-    parser.add_argument(
-        "--active_prob_const",
-        type=float,
-        default=0.5,
-        help="Constant value for the active probability.",
-    )
-    parser.add_argument(
-        "--n_const",
-        type=int,
-        default=150,
-        help="Constant value for the block length.",
-    )
-    parser.add_argument(
-        "--k_const",
-        type=int,
-        default=100,
-        help="Constant value for the update size.",
-    )
-    parser.add_argument(
-        "--P_const",
-        type=float,
-        default=2 * (10**-3),
-        help="Constant value for the power.",
-    )
-    parser.add_argument(
-        "--d_const",
-        type=int,
-        default=700,
-        help="Constant value for the distance between nodes.",
-    )
-    parser.add_argument(
-        "--N0_const",
-        type=float,
-        default=1 * (10**-13),
-        help="Constant value for the noise power.",
-    )
-    parser.add_argument(
-        "--fr_const",
-        type=float,
-        default=6 * (10**9),
-        help="Constant value for the frequency of the signal.",
-    )
-    parser.add_argument(
-        "--numevnts", type=int, default=500, help="The number of events."
-    )
-    parser.add_argument("--numruns", type=int, default=100, help="The number of runs.")
-
-    parser.add_argument(
-        "--num_nodes_vals",
-        nargs="+",
-        type=int,
-        default=[1, 2, 3, 4, 5],
-        help="Values for the number of nodes.",
-    )
-    parser.add_argument(
-        "--active_prob_vals",
-        nargs="+",
-        type=float,
-        default=[0.1, 0.15, 0.2, 0.25],
-        help="Values for the active probability.",
-    )
-    parser.add_argument(
-        "--n_vals",
-        nargs="+",
-        type=int,
-        default=[150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250],
-        help="Values for the block length.",
-    )
-    parser.add_argument(
-        "--k_vals",
-        nargs="+",
-        type=int,
-        default=[50, 60, 70, 80, 90, 95, 100],
-        help="Values for the update size.",
-    )
-    parser.add_argument(
-        "--P_vals",
-        nargs="+",
-        type=float,
-        default=[
-            2 * (10**-3),
-            4 * (10**-3),
-            6 * (10**-3),
-            8 * (10**-3),
-        ],
-        help="Values for the power.",
-    )
+    
+    parser.add_argument("--d", nargs="+", type=float, default=[700],
+                        help="List of distances between nodes.")
+    parser.add_argument("--N0", nargs="+", type=float, default=[1e-13],
+                        help="List of noise power values.")
+    parser.add_argument("--fr", nargs="+", type=float, default=[6e9],
+                        help="List of signal frequencies.")
+    parser.add_argument("--numevnts", nargs="+", type=int, default=[5,10],
+                        help="List of number of events.")
+    parser.add_argument("--num_nodes", nargs="+", type=int, default=[5],
+                        help="List of number of nodes.")
+    parser.add_argument("--active_prob", nargs="+", type=float, default=[0.2],
+                        help="List of active probabilities.")
+    parser.add_argument("--n", nargs="+", type=int, 
+                        default=[150],
+                        help="List of block lengths.")
+    parser.add_argument("--k", nargs="+", type=int, default=[50],
+                        help="List of update sizes.")
+    parser.add_argument("--P", nargs="+", type=float, 
+                        default=[8e-3],
+                        help="List of power values.")
+    parser.add_argument("--numruns", type=int, default=10,
+                        help="Number of runs for each parameter combination.")
+    parser.add_argument("--seed", type=int, default=None,
+                        help="Random seed for reproducibility.")
 
     parser.add_argument("--quiet", action="store_true", help="Omit tables")
     parser.add_argument("--plots", action="store_true", help="Show plots")
     parser.add_argument("--plots_folder", type=str, help="Folder to save plots")
-    parser.add_argument(
-        "--blockerror", action="store_true", help="Show theoretical block error"
-    )
+    parser.add_argument("--blockerror", action="store_true", help="Show theoretical block error")
     parser.add_argument("--snr", action="store_true", help="Show snr")
     parser.add_argument("--csv_location", type=str, help="Location to save csv file")
-
-    # Add new argument for seed
-    parser.add_argument(
-        "--seed",
-        type=int,
-        default=None,
-        help="Seed for the random number generator. If not provided, a random seed will be used.",
-    )
 
     args = parser.parse_args()
 
     output_action_taken = False
 
-    # if args.plots or args.plots_folder:
-    #     plot(args, args.plots_folder if args.plots_folder else None, seed=args.seed)
-    #     output_action_taken = True
-
     if args.snr:
-        snr_th_val = snr_th(args.N0_const, args.d_const, args.P_const, args.fr_const)
+        # Note: This might need adjustment as we don't have single values for these parameters anymore
+        snr_th_val = snr_th(args.N0[0], args.d[0], args.P[0], args.fr[0])
         print(f"Theoretical SNR: {snr_th_val}")
         output_action_taken = True
 
     if args.blockerror:
-        ber_th = blercal_th(args.snr, args.n_const, args.k_const)
+        # Note: This might need adjustment as we don't have single values for these parameters anymore
+        ber_th = blercal_th(args.snr, args.n[0], args.k[0])
         print(f"Theoretical Block Error Rate: {ber_th}")
         output_action_taken = True
 
-#  if not output_action_taken and not args.quiet:
-#         generate_table(
-#             args.num_nodes_const,
-#             args.active_prob_const,
-#             args.n_const,
-#             args.k_const,
-#             args.P_const,
-#             args.d_const,
-#             args.N0_const,
-#             args.fr_const,
-#             args.numevnts,
-#             args.numruns,
-#             args.num_nodes_vals,
-#             args.active_prob_vals,
-#             args.n_vals,
-#             args.k_vals,
-#             args.P_vals,
-#             args.csv_location if args.csv_location else None,
-#             seed=args.seed,
-#         )
+    if not output_action_taken and not args.quiet:
+        results = multi_param_ev_sim(
+            d=args.d,
+            N0=args.N0,
+            fr=args.fr,
+            numevnts=args.numevnts,
+            num_nodes=args.num_nodes,
+            active_prob=args.active_prob,
+            n=args.n,
+            k=args.k,
+            P=args.P,
+            numruns=args.numruns,
+            seed=args.seed
+        )
+        print(results)
