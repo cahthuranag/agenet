3cad9e6f30e1f2b889c9f1ce913924c19285a8e3
Nuno Fachada <faken@fakenmc.com>
2024-10-11 19:16:04 +0100

    CLI: show defaults

--- agenet/cli.py
+++ agenet/cli.py
@@ -10,6 +10,23 @@ from .snratio import snr_th
 
 
 def _main():
+
+    def_params = {
+        "distance": [100],
+        "N0": [1e-13],
+        "frequency": [6e6],
+        "num-events": [50],
+        "num-nodes": [5],
+        "active-prob": [0.2],
+        "num-bits": [150],
+        "info-bits": [50],
+        "power": [7e-2, 8e-2, 9e-2],
+    }
+
+    def_params_str = {
+        key: " ".join(map(str, value)) for key, value in def_params.items()
+    }
+
     # Create a parser to parse our command line arguments
     parser = argparse.ArgumentParser(
         prog="agenet", description="Command line interface to AgeNet"
@@ -21,62 +38,72 @@ def _main():
         "--distance",
         type=float,
         nargs="+",
-        default=[100],
-        help="Distance between nodes (meters)",
+        default=def_params["distance"],
+        help=f"Distance between nodes in meters (default: {def_params_str["distance"]})",
     )
     parser.add_argument(
         "-N",
         "--N0",
         type=float,
         nargs="+",
-        default=[1e-13],
-        help="Noise power (Watts)",
+        default=def_params["N0"],
+        help=f"Noise power in Watts (default: {def_params_str["N0"]})",
     )
     parser.add_argument(
         "-f",
         "--frequency",
         type=float,
         nargs="+",
-        default=[6e6],
-        help="Signal frequency (Hz)",
+        default=def_params["frequency"],
+        help=f"Signal frequency in Hz (default: {def_params_str["frequency"]})",
     )
     parser.add_argument(
-        "-e", "--num-events", type=int, nargs="+", default=[50], help="Number of events"
+        "-e",
+        "--num-events",
+        type=int,
+        nargs="+",
+        default=def_params["num-events"],
+        help=f"Number of events (default: {def_params_str["num-events"]})",
     )
     parser.add_argument(
-        "-m", "--num-nodes", type=int, nargs="+", default=[5], help="Number of nodes"
+        "-m",
+        "--num-nodes",
+        type=int,
+        nargs="+",
+        default=def_params["num-nodes"],
+        help=f"Number of nodes (default: {def_params_str["num-nodes"]})",
     )
     parser.add_argument(
         "-a",
         "--active-prob",
         type=float,
         nargs="+",
-        default=[0.2],
-        help="Active probability",
+        default=def_params["active-prob"],
+        help=f"Active probability (default: {def_params_str["active-prob"]})",
     )
     parser.add_argument(
         "-n",
         "--num-bits",
         type=int,
         nargs="+",
-        default=[150],
-        help="Total number of bits",
+        default=def_params["num-bits"],
+        help=f"Total number of bits (default: {def_params_str["num-bits"]})",
     )
     parser.add_argument(
         "-k",
         "--info-bits",
         type=int,
         nargs="+",
-        default=[50],
-        help="Number of information bits",
+        default=def_params["info-bits"],
+        help=f"Number of information bits (default: {def_params_str["info-bits"]})",
     )
     parser.add_argument(
         "-P",
         "--power",
         type=float,
         nargs="+",
-        default=[8e-2],
-        help="Transmission power (Watts)",
+        default=def_params["power"],
+        help=f"Transmission power in Watts (default: {def_params_str["power"]})",
     )
     parser.add_argument(
         "-r", "--num-runs", type=int, default=10, help="Number of simulation runs"
@@ -106,8 +133,8 @@ def _main():
 
     args = parser.parse_args()
 
-    # Always run the simulation
-    result = multi_param_ev_sim(
+    # Run the simulations
+    results = multi_param_ev_sim(
         distance=args.distance,
         N0=args.N0,
         frequency=args.frequency,
@@ -116,7 +143,7 @@ def _main():
         active_prob=args.active_prob,
         num_bits=args.num_bits,
         info_bits=args.info_bits,
-        power=args.dep_prob,
+        power=args.power,
         num_runs=args.num_runs,
         seed=args.seed,
     )
@@ -125,21 +152,19 @@ def _main():
     if not args.quiet:
 
         theoretical_snr = snr_th(
-            args.N0[0], args.distance[0], args.dep_prob[0], args.frequency[0]
+            args.N0[0], args.distance[0], args.power[0], args.frequency[0]
         )
-        if not args.quiet:
-            print(f"Theoretical SNR: {theoretical_snr}")
+        print(f"Theoretical SNR: {theoretical_snr}")
 
         theoretical_bler = block_error_th(
-            args.num_bits[0], args.info_bits[0], args.dep_prob[0]
+            args.num_bits[0], args.info_bits[0], args.power[0]
         )
-        if not args.quiet:
-            print(f"Theoretical Block Error Rate: {theoretical_bler}")
+        print(f"Theoretical Block Error Rate: {theoretical_bler}")
 
-        print(result)
+        print(results)
 
     if args.csv:
-        result.to_csv(args.csv, index=False)
+        results.to_csv(args.csv, index=False)
         print(f"Simulation results saved to {args.csv}")
 
     if args.plot_show or len(args.plot_save) > 0:
@@ -154,7 +179,7 @@ def _main():
             ("active_prob", "Active probability", args.active_prob),
             ("num_bits", "n - Number of bits", args.num_bits),
             ("info_bits", "k - Information bits", args.info_bits),
-            ("power", "P - Transmission power (W)", args.dep_prob),
+            ("power", "P - Transmission power (W)", args.power),
         ]:
 
             if len(param_info[2]) > 1:
@@ -162,13 +187,13 @@ def _main():
                 aoi_vs_param = param_info
 
         if num_var_params == 1:
-            result_sorted = result.sort_values(aoi_vs_param[0])
+            results_sorted = results.sort_values(aoi_vs_param[0])
 
             fig, ax = plt.subplots()
-            aaoi_theory = result_sorted["aaoi_theory"]
-            aaoi_sim = result_sorted["aaoi_sim"]
-            ax.plot(result_sorted[aoi_vs_param[0]], aaoi_theory, label="Theoretical")
-            ax.plot(result_sorted[aoi_vs_param[0]], aaoi_sim, label="Simulation")
+            aaoi_theory = results_sorted["aaoi_theory"]
+            aaoi_sim = results_sorted["aaoi_sim"]
+            ax.plot(results_sorted[aoi_vs_param[0]], aaoi_theory, label="Theoretical")
+            ax.plot(results_sorted[aoi_vs_param[0]], aaoi_sim, label="Simulation")
             ax.set_xlabel(aoi_vs_param[1])
             ax.set_ylabel("AAoI")
             ax.set_ylim([0, max(aaoi_theory.max(), aaoi_sim.max()) * 1.05])
