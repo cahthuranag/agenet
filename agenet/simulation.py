c763b02d133619755a0fe8323d55c670de1fc974
Nuno Fachada <nuno.fachada@ulusofona.pt>
2024-10-16 16:48:50 +0100

    Move simulation code to low level _sim() function

--- agenet/simulation.py
+++ agenet/simulation.py
@@ -1,4 +1,4 @@
-"""Main file for the communication system simulation."""
+"""Module containing core communication system simulation."""
 
 from __future__ import annotations
 
@@ -13,6 +13,100 @@ from numpy.random import PCG64, PCG64DXSM, Generator, Philox
 from .aaoi import aaoi_fn
 from .blkerr import block_error, block_error_th
 from .snratio import snr, snr_av
+
+
+def _sim(
+    frequency: float,
+    num_events: int,
+    num_bits_1: int,
+    info_bits_1: int,
+    power_1: float,
+    distance_1: float,
+    n0_1: float,
+    blkerr1_th: float,
+    num_bits_2: int,
+    info_bits_2: int,
+    power_2: float,
+    distance_2: float,
+    n0_2: float,
+    blkerr2_th: float,
+    rng: Generator,
+) -> tuple[float, float]:
+    """Low-level function for simulating a communication system and obtaining the AAoI.
+
+    Args:
+      TODO
+
+    Returns:
+       TODO
+    """
+    # symbol time
+    symbol_time = 60e-6
+
+    # Transmission period
+    transmission_period = (num_bits_1 + num_bits_2) * symbol_time
+
+    # Inter-arrival times
+    inter_arrival_times = transmission_period * (np.ones(num_events))
+
+    # Arrival timestamps
+    arrival_timestamps = np.cumsum(inter_arrival_times)
+
+    # Inter-service times
+    inter_service_times = transmission_period * np.ones((num_events))
+
+    # Generating departure timestamps for node 1
+    server_timestamps_1 = np.zeros(num_events)
+    departure_timestamps_s = np.zeros(num_events)
+
+    er_p_th = blkerr1_th + (blkerr2_th * (1 - blkerr1_th))
+
+    for i in range(0, num_events):
+
+        # SNR for the source nodes at the relay or access point
+        snr1 = snr(n0_1, distance_1, power_1, frequency, seed=rng.integers(0, 2**32))
+        snr2 = snr(n0_2, distance_2, power_2, frequency, seed=rng.integers(0, 2**32))
+
+        # block error rate for the source nodes at the relay or access point
+        er1 = block_error(snr1, num_bits_1, info_bits_1)
+
+        # block error rate for the relay or access point at the destination
+        er2 = block_error(snr2, num_bits_2, info_bits_2)
+
+        er_p = er1 + (er2 * (1 - er1))
+        er_indi = int(rng.random() > er_p)
+
+        if er_indi == 0:
+            # If the packet is not successfully decoded at the destination,
+            # departure timestamp is set to nan
+            departure_timestamps_s[i] = nan
+            server_timestamps_1[i] = nan
+        else:
+            departure_timestamps_s[i] = arrival_timestamps[i] + inter_service_times[i]
+            server_timestamps_1[i] = arrival_timestamps[i]
+
+    dep = [x for x in departure_timestamps_s if not np.isnan(x)]
+    sermat = [x for x in server_timestamps_1 if not np.isnan(x)]
+
+    # Choose a small threshold
+    if abs(1 - er_p_th) < 1e-20:
+        return float("inf"), float("inf")
+
+    aaoi_th = (transmission_period) * (0.5 + (1 / (1 - er_p_th)))
+
+    # if dep and sermat are empty, return infinity
+    if not dep or not sermat:
+        return float("inf"), float("inf")
+    if departure_timestamps_s[-1] == nan:
+        depature_mat = dep + arrival_timestamps[-1] + inter_service_times[-1]
+        arrival_mat = [0] + sermat[1:] + arrival_timestamps[-1]
+    else:
+        depature_mat = dep
+        arrival_mat = [0] + sermat[1:]
+
+    aaoi_sim, _, _ = aaoi_fn(depature_mat, arrival_mat)
+
+    return aaoi_th, aaoi_sim
 
 
 def sim(
@@ -89,78 +183,42 @@ def sim(
     # Initialize PCG64 generator
     rng = Generator(PCG64(seed))
 
-    # symbol time
-    symbol_time = 60e-6
-    # Transmission period
-    transmission_period = (n1 + n2) * symbol_time
-    # Inter-arrival times
-    inter_arrival_times = transmission_period * (np.ones(num_events))
-    # Arrival timestamps
-    arrival_timestamps = np.cumsum(inter_arrival_times)
-
-    snr1_th = snr_av(np01, d1, p1, frequency)
-
-    snr2_th = snr_av(np02, d2, p2, frequency)
+    # Determine the average SNR for the two nodes
+    snr1_avg = snr_av(np01, d1, p1, frequency)
+    snr2_avg = snr_av(np02, d2, p2, frequency)
 
     # Block error rate for the relay or access point at the destination
-    er1_th = block_error_th(snr1_th, n1, k1)
+    # TODO: Check above comment for correctness
+    er1_th = block_error_th(snr1_avg, n1, k1)
 
     # Block error rate for the source node at the relay or access point
-    er2_th = block_error_th(snr2_th, n2, k2)
+    # TODO: Check above comment for correctness
+    er2_th = block_error_th(snr2_avg, n2, k2)
 
-    # Inter-service times
-    inter_service_times = transmission_period * np.ones((num_events))
-
-    # Generating departure timestamps for node 1
-    server_timestamps_1 = np.zeros(num_events)
-
-    departure_timestamps_s = np.zeros(num_events)
-    er_p_th = er1_th + (er2_th * (1 - er1_th))
-    for i in range(0, num_events):
-
-        # SNR for the source nodes at the relay or access point
-        snr1 = snr(N0, d1, p1, frequency, seed=rng.integers(0, 2**32))
-        snr2 = snr(N0, d2, p2, frequency, seed=rng.integers(0, 2**32))
-
-        # block error rate for the source nodes at the relay or access point
-        er1 = block_error(snr1, n1, k1)
-
-        # block error rate for the relay or access point at the destination
-        er2 = block_error(snr2, n2, k2)
-        er_p = er1 + (er2 * (1 - er1))
-        er_indi = int(rng.random() > er_p)
-
-        if er_indi == 0:
-            # If the packet is not successfully decoded at the destination,
-            # departure timestamp is set to nan
-            departure_timestamps_s[i] = nan
-            server_timestamps_1[i] = nan
-        else:
-            departure_timestamps_s[i] = arrival_timestamps[i] + inter_service_times[i]
-            server_timestamps_1[i] = arrival_timestamps[i]
-
-    dep = [x for x in departure_timestamps_s if not np.isnan(x)]
-    sermat = [x for x in server_timestamps_1 if not np.isnan(x)]
-
-    # Choose a small threshold
-    if abs(1 - er_p_th) < 1e-20:
-        return float("inf"), float("inf"), snr1_th, snr2_th, er1_th, er2_th
-
-    aaoi_theoretical = (transmission_period) * (0.5 + (1 / (1 - er_p_th)))
-
-    # if dep and sermat are empty, return infinity
-    if not dep or not sermat:
-        return float("inf"), float("inf"), snr1_th, snr2_th, er1_th, er2_th
-    if departure_timestamps_s[-1] == nan:
-        depature_mat = dep + arrival_timestamps[-1] + inter_service_times[-1]
-        arrival_mat = [0] + sermat[1:] + arrival_timestamps[-1]
-    else:
-        depature_mat = dep
-        arrival_mat = [0] + sermat[1:]
-
-    aaoi_simulation, _, _ = aaoi_fn(depature_mat, arrival_mat)
-
-    return aaoi_theoretical, aaoi_simulation, snr1_th, snr2_th, er1_th, er2_th
+    # Call the low-level function to actually perform the simulation
+    return (
+        *_sim(
+            frequency=frequency,
+            num_events=num_events,
+            num_bits_1=n1,
+            info_bits_1=k1,
+            power_1=p1,
+            distance_1=d1,
+            n0_1=np01,
+            blkerr1_th=er1_th,
+            num_bits_2=n2,
+            info_bits_2=k2,
+            power_2=p2,
+            distance_2=d2,
+            n0_2=np02,
+            blkerr2_th=er2_th,
+            rng=rng,
+        ),
+        snr1_avg,
+        snr2_avg,
+        er1_th,
+        er2_th,
+    )
 
 
 def ev_sim(
