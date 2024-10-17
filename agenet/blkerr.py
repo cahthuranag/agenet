5a892c5068c664a5df9ffc147be408c9169530c4
Nuno Fachada <nuno.fachada@ulusofona.pt>
2024-10-17 22:59:46 +0100

    Improvements on naming, docstrings, and test imports

--- agenet/blkerr.py
+++ agenet/blkerr.py
@@ -9,19 +9,19 @@ def _qfunc(x: float) -> float:
     """The Q-function gives the tail probability of the std."""
     if x < 0:
         return 1
-    return 0.5 - 0.5 * sp.erf(x / math.sqrt(2))  # pylint: disable=E1101
+    return 0.5 - 0.5 * sp.erf(x / math.sqrt(2))
 
 
 def block_error(snr: float, n: int, k: int) -> float:
-    """Calculate the Block Error Rate (BLER) for the given instantaneous  SNR, n, k.
+    """Calculate the Block Error Rate for the given instantaneous SNR, n, k.
 
     Args:
-        snr (float): Instantaneous signal-to-noise ratio
-        n (int): Total number of bits
-        k (int): Number of information bits
+        snr: Instantaneous signal-to-noise ratio.
+        n: Total number of bits.
+        k: Number of information bits.
 
     Returns:
-        float: The Block Error Rate
+        The Block Error Rate.
     """
     c = math.log2(1 + snr)
     v = 0.5 * ((1 - (1 / ((1 + snr) ** 2))) * ((math.log2(math.exp(1))) ** 2))
@@ -34,16 +34,16 @@ def block_error(snr: float, n: int, k: int) -> float:
     return err
 
 
-def block_error_th(snr_av: float, n: int, k: int) -> float:
+def block_error_th(snr_avg: float, n: int, k: int) -> float:
     """Calculate the theoretical Block Error Rate for the given average SNR, n, k.
 
     Args:
-        snr_av: Average Signal-to-noise ratio
-        n: Total number of bits
-        k: Number of information bits
+        snr_avg: Average Signal-to-noise ratio.
+        n: Total number of bits.
+        k: Number of information bits.
 
     Returns:
-        float: The theoretical Block Error Rate
+        The theoretical Block Error Rate.
     """
     try:
         beta = 1 / (2 * math.pi * math.sqrt((2 ** (2 * k / n)) - 1))
@@ -55,7 +55,10 @@ def block_error_th(snr_av: float, n: int, k: int) -> float:
     delta = sim_phi + (1 / (2 * beta * math.sqrt(n)))
 
     err_th = 1 - (
-        (beta * math.sqrt(n) * snr_av)
-        * (math.exp(-1 * phi_bas * (1 / snr_av)) - math.exp(-1 * delta * (1 / snr_av)))
+        (beta * math.sqrt(n) * snr_avg)
+        * (
+            math.exp(-1 * phi_bas * (1 / snr_avg))
+            - math.exp(-1 * delta * (1 / snr_avg))
+        )
     )
     return err_th
--- test/test_blkerr.py
+++ test/test_blkerr.py
@@ -1,14 +1,12 @@
-"""Tests for the bler module."""
+"""Tests for the blkerr module."""
 
 import math
 
 import numpy as np
 import pytest
-import scipy.special as sp
 
 from agenet import block_error, block_error_th
-
-# Test block_error() function
+from agenet.blkerr import _qfunc
 
 
 def test_block_error():
@@ -28,9 +26,6 @@ def test_block_error():
     assert np.isclose(block_error(100, 100, 50), 0.0, rtol=1e-2)
 
 
-# Test block_error_th() function
-
-
 def test_block_error_th():
     """Test the block_error_th function for some known inputs and expected outputs."""
     # Test for small SNR
@@ -40,22 +35,15 @@ def test_block_error_th():
     assert np.isclose(round(block_error_th(1000, 600, 100), 3), 0)
 
 
-def qfunc(x):
-    """Calculates the Q-function."""
-    if x < 0:
-        return 1
-    return 0.5 - 0.5 * sp.erf(x / math.sqrt(2))
-
-
 def test_qfunc():
     """Test the qfunc function for some known inputs and expected outputs."""
     # Test the qfunc function for some known inputs and expected outputs
-    assert qfunc(-10) == 1
-    assert qfunc(-1) == 1
-    assert qfunc(0) == 0.5
-    assert qfunc(1) == 0.15865525393145707
+    assert _qfunc(-10) == 1
+    assert _qfunc(-1) == 1
+    assert _qfunc(0) == 0.5
+    assert _qfunc(1) == 0.15865525393145707
 
     # Test the qfunc function for some edge cases
-    assert math.isnan(qfunc(float("nan")))
-    assert qfunc(float("-inf")) == 1
-    assert qfunc(float("inf")) == 0
+    assert math.isnan(_qfunc(float("nan")))
+    assert _qfunc(float("-inf")) == 1
+    assert _qfunc(float("inf")) == 0
