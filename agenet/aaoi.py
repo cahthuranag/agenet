6c57305fae420de648171dcbe52020db44bf2ac8
cahthuranag <chathurangab@sltc.ac.lk>
2024-10-18 16:33:17 +0100

    Changed paramters in the aaoi function to make consistent

--- agenet/aaoi.py
+++ agenet/aaoi.py
@@ -8,24 +8,24 @@ from scipy.integrate import trapezoid
 
 
 def aaoi_fn(
-    departure_timestamps: NDArray, final_arrival_times: NDArray
+    departure_times: NDArray, final_arrival_times: NDArray
 ) -> tuple[float, NDArray, NDArray]:
     """Calculate the average age of information.
 
     Args:
-        departure_timestamps: List of departure timestamps.
+        departure_times: List of departure times.
         final_arrival_times: List of final arrival times.
 
     Returns:
         Average age of information, age, times.
     """
     # Generate times for the time axis
-    times: NDArray = np.arange(0, departure_timestamps[0] + 0.0005, 0.0005)
-    num_events = len(departure_timestamps)
+    times: NDArray = np.arange(0, departure_times[0] + 0.0005, 0.0005)
+    num_events = len(departure_times)
 
     for i in range(1, num_events):
         dummy = np.arange(
-            departure_timestamps[i - 1], departure_timestamps[i] + 0.0001, 0.0001
+            departure_times[i - 1], departure_times[i] + 0.0001, 0.0001
         )
         times = np.concatenate((times, dummy))
 
@@ -35,7 +35,7 @@ def aaoi_fn(
     age = times.copy()
 
     for i in range(len(times)):
-        if times[i] == departure_timestamps[j]:
+        if times[i] == departure_times[j]:
             offset = final_arrival_times[j]
             j += 1
         age[i] -= offset
